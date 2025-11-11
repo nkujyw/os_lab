4042f6e47968aba638eb9628c81ec832a16d8b65
KK467 <106748927+KK19212575@users.noreply.github.com>
2025-11-11 20:08:16 +0800

    练习2

--- lab4/代码/kern/process/proc.c
+++ lab4/代码/kern/process/proc.c
@@ -297,7 +297,7 @@ int do_fork(uint32_t clone_flags, uintptr_t stack, struct trapframe *tf)
         goto fork_out;
     }
     ret = -E_NO_MEM;
-    // LAB4:EXERCISE2 YOUR CODE
+    // LAB4:EXERCISE2 2313109
     /*
      * Some Useful MACROs, Functions and DEFINEs, you can use them in below implementation.
      * MACROs or Functions:
@@ -322,7 +322,30 @@ int do_fork(uint32_t clone_flags, uintptr_t stack, struct trapframe *tf)
     //    5. insert proc_struct into hash_list && proc_list
     //    6. call wakeup_proc to make the new child process RUNNABLE
     //    7. set ret vaule using child proc's pid
-    
+        if ((proc = alloc_proc()) == NULL)
+    {
+        goto fork_out;
+    }
+    if (setup_kstack(proc) != 0)
+    {
+        goto bad_fork_cleanup_proc;
+    }
+    if (copy_mm(clone_flags, proc) != 0)
+    {
+        goto bad_fork_cleanup_kstack;
+    }
+
+    proc->parent = current;
+    proc->pid = get_pid();
+
+    copy_thread(proc, stack, tf);
+    hash_proc(proc);
+    list_add(&proc_list, &(proc->list_link));
+
+    wakeup_proc(proc);
+    nr_process++;
+    ret = proc->pid;
+
 fork_out:
     return ret;
 
